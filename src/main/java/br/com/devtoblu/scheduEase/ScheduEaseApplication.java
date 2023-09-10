b3284f9f9d71c830454cd68a613d295ecc326352
NeoJhonn <jhonny.azevedo@gmail.com>
2023-09-10 18:49:37 -0300

    Added MVP

---
+++ src/main/java/br/com/devtoblu/scheduEase/ScheduEaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.devtoblu.scheduEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduEaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devtoblu/scheduEase/ScheduEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devtoblu.scheduEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
